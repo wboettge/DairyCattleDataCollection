2b9867be8354f1d0e4252709e02b3a84062faa4f
wboettge <wboettge@wisc.edu>
2021-11-17 21:46:26 -0600

    Initial commit

---
+++ test/dairy_cattle_data_collection.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as DairyCattleDataCollection from '../lib/dairy_cattle_data_collection-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dairy_cattle_data_collection-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DairyCattleDataCollection.DairyCattleDataCollectionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
