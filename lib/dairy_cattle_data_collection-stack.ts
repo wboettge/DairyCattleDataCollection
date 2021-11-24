8022faf007a18a10ebb88befd4ff626fd5c7c48f
wboettge <wboettge@wisc.edu>
2021-11-23 18:48:23 -0600

    make db and table cdk resources

--- lib/dairy_cattle_data_collection-stack.ts
+++ lib/dairy_cattle_data_collection-stack.ts
@@ -1,13 +1,35 @@
 import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
 import * as cfninc from '@aws-cdk/cloudformation-include';
+import * as timestream from '@aws-cdk/aws-timestream';
+import * as iot from '@aws-cdk/aws-iot';
+import * as actions from '@aws-cdk/aws-iot-actions';
+
 
 export class DairyCattleDataCollectionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const template = new cfninc.CfnInclude(this, 'Template', { 
-      templateFile: path.join(__dirname, 'PipelineStack.yaml'),
+    const cfnDatabase = new timestream.CfnDatabase(this, 'TimeStreamDatabase', {
+      databaseName: 'CFTestDB',
     });
+
+    const cfnTable = new timestream.CfnTable(this, 'TimeStreamTable', {
+      databaseName: 'CFTestDB',
+      retentionProperties: {
+        'MemoryStoreRetentionPeriodInHours': '24',
+        'MagneticStoreRetentionPeriodInDays': '7'
+      },
+      tableName: 'CFTestTable'
+    });
+
+    const timestreamRuleAction = new cfninc.CfnInclude(this, 'Template', { 
+      templateFile: path.join(__dirname, 'PipelineStack.yaml'),
+      parameters: {
+        'TimeStreamTableARN': cfnTable.attrArn
+      }
+    });
+
+
   }
 }
