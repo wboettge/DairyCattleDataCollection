3e433df1a31c612be97723a5327c1ff0a5c01c41
wboettge <wboettge@wisc.edu>
2022-03-03 11:48:48 -0600

    update comments with TODOs

--- lib/dairy_cattle_data_collection-stack.ts
+++ lib/dairy_cattle_data_collection-stack.ts
@@ -29,6 +29,7 @@ export class DairyCattleDataCollectionStack extends cdk.Stack {
 
     // No cdk support yet for IoT topic rule actions. Therefore the resource
     // and IAM role is defined in a CloudFormation configuration file
+    // TODO cdk support now exists, so use the constructs instead of the CFN
     const timestreamRuleAction = new cfninc.CfnInclude(this, 'Template', { 
       templateFile: path.join(__dirname, 'iot_rule_action.yaml'),
       parameters: {
@@ -37,7 +38,6 @@ export class DairyCattleDataCollectionStack extends cdk.Stack {
     });
 
     // Define an EC2 instance
-    // TODO use default VPC
     const vpc = new ec2.Vpc(this, 'VPC', {
       subnetConfiguration: [{
         name: "public-subnet",
@@ -73,7 +73,7 @@ export class DairyCattleDataCollectionStack extends cdk.Stack {
       machineImage: new ec2.AmazonLinuxImage({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
       }),
-      keyName: 'AWS',
+      keyName: 'AWS', // TODO create our own keys
       init: ec2.CloudFormationInit.fromElements(
         ec2.InitFile.fromFileInline('/etc/grafana/provisioning/dashboards/dashboards.yaml', './lib/Grafana/dashboards.yaml'),
         ec2.InitFile.fromFileInline('/etc/grafana/provisioning/datasources/datasources.yaml', './lib/Grafana/datasources.yaml'),
